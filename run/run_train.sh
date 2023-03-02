b89bd12dda9ee6c9f4ae0a5f3fe84786e0e5d542
Byoungjun Park <bingjun5249@cms02.knu.ac.kr>
2023-03-02 14:20:16 +0900

    correction

--- run/run_train.sh
+++ run/run_train.sh
@@ -5,12 +5,5 @@ lr=0.01
 python train.py --epoch $epoch --batch $batch --lr $lr
 python Eval.py --epoch $epoch --batch $batch --lr $lr
 
-count=`ls -l ../storage/run_files/total/|grep ^d|wc -l`
-count=`expr $count + 1`
-echo $count
 
-
-mkdir ../storage/run_files/total/trial$count/
-
-mv total* ../storage/run_files/total/trial$count/
-
+source hand_out.sh
